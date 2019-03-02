acaa9d8b39c751d405f96e56a7cf8c412807347b
Shaun <torianharris917@gmail.com>
2019-03-02 10:17:11 -0500

    table setup

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 // var keys = require("../keys.js");
 var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection;
+if(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+else {
+  connection =  mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.SQL_PASS,
+    database: 'burgers_db'
+  })
+}
+
 
 connection.connect();
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
